af4918f7f750ef7849d03e6f48127a8af10bae01
John Kevin Losito <48610130+johnkevinlosito@users.noreply.github.com>
2022-05-10 23:17:58 +0800

    section 1-4

---
+++ Landmarks/Landmarks/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by John Kevin Losito on 5/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Turtle Rock")
+                .font(.title)
+            HStack {
+                Text("Joshua Tree National Park")
+                    .font(.subheadline)
+                Spacer()
+                Text("California")
+                    .font(.subheadline)
+            }
+        }
+        .padding()
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by John Kevin Losito on 5/10/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
