3a8bdd596c7f3512041b2869e94710a88ad6fe6b
John Kevin Losito <48610130+johnkevinlosito@users.noreply.github.com>
2022-05-15 22:57:12 +0800

    toggle favorites

--- Landmarks/Landmarks/Views/LandmarkRow.swift
+++ Landmarks/Landmarks/Views/LandmarkRow.swift
@@ -17,6 +17,10 @@ struct LandmarkRow: View {
                 .frame(width: 50, height: 50)
             Text(landmark.name)
             Spacer()
+            if landmark.isFavorite {
+                Image(systemName: "star.fill")
+                    .foregroundColor(.yellow)
+            }
         }
     }
 }
@@ -25,7 +29,7 @@ struct LandmarkRow_Previews: PreviewProvider {
     static var previews: some View {
         Group {
             LandmarkRow(landmark: landmarks[0])
-                
+            
             LandmarkRow(landmark: landmarks[1])
         }.previewLayout(.fixed(width: 300, height: 70))
     }
